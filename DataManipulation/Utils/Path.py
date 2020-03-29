52a75f507d495673444f36e432189deaa2f9dc4b
simocampi <Sim0ne1997>
2020-03-29 11:37:57 +0200

    Path and Object Demographic Info

--- DataManipulation/Utils/Path.py
+++ DataManipulation/Utils/Path.py
@@ -1,10 +1,11 @@
 class Path():
     
+    #Get Path of Project directory
     @staticmethod
     def  get_database_path():
         file_path= __file__
         
         head, sep, tail = file_path.partition('LSCproject\\')
-        print('\n'+head+sep+'\n')
+        project_path = head+sep
 
-        return head+sep+'Database\\'
+        return project_path+'Database\\'
