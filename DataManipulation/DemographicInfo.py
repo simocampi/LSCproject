82844aaf00293649a19421b91b8e4c923e1870c6
simocampi <Sim0ne1997>
2020-03-29 15:02:52 +0200

    Co-authored-by: Alessandro <alessandro.caroti.1996@gmail.com>

--- DataManipulation/DemographicInfo.py
+++ DataManipulation/DemographicInfo.py
@@ -1,7 +1,14 @@
-import sys,os
-
-from Utils.Path import Path
+from DataManipulation.Utils.Path import Path
 
 class DemographicInfo(object):
-    DEMOGRAPHIC_INFO_FILE = 'demographic_info.txt'
-    DEMOGRAPHIC_INFO_PATH = Path.get_database_path()+DEMOGRAPHIC_INFO_FILE
+    
+    def __init__(self, spark_context):
+        
+        DEMOGRAPHIC_INFO_FILE = 'demographic_info.txt'
+        DEMOGRAPHIC_INFO_PATH = Path.get_database_path()+DEMOGRAPHIC_INFO_FILE
+
+        self.spark_context = spark_context
+        self.rdd = spark_context.textFile(DEMOGRAPHIC_INFO_PATH)
+
+    def get_rdd(self):
+        return self.rdd
---
+++ Main.py
@@ -0,0 +1,27 @@
+from pyspark import SparkContext
+from pyspark.sql.functions import *
+from pyspark.sql.functions import col
+from pyspark.sql.window import Window
+from pyspark.sql import Row, functions as F
+from DataManipulation.DemographicInfo import DemographicInfo
+import sys
+import os
+from importlib import reload
+from DataManipulation.Utils.Path import Path
+
+spark_context= SparkContext(
+        master = 'local',
+        appName = 'LSC_PROJECT', 
+        sparkHome = None, 
+        pyFiles = None, 
+        environment = None, 
+        batchSize = 0, 
+        conf = None, 
+        gateway = None, 
+        jsc = None
+)
+
+
+demographic_info = DemographicInfo(spark_context)
+
+rdd_demographic_info = demographic_info.get_rdd()
