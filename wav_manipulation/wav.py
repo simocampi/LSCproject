4667437c106cd10d03b7bc41e4dd4f90055ee2f5
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-04-05 12:47:38 +0200

    create DF with fileName

--- wav_manipulation/wav.py
+++ wav_manipulation/wav.py
@@ -2,6 +2,12 @@ from os import *
 from os.path import *
 from Utils.Path import *
 from pyspark.sql.types import (StructField,StringType,IntegerType,StructType)
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark Exercise 5") \
+    .getOrCreate()
 
 class WAV:
     
@@ -10,9 +16,12 @@ class WAV:
 
     @staticmethod
     def wav_files():
-        wav_files = [f[:-4] for f in listdir(WAV.PATH_FILES_WAV) if (isfile(join(WAV.PATH_FILES_WAV, f)) and f.endswith('.wav'))] 
-        for f in wav_files:
-            print(f)
+        wav_files = [[f] for f in listdir(WAV.PATH_FILES_WAV) if (isfile(join(WAV.PATH_FILES_WAV, f)) and f.endswith('.wav'))] 
+
+        cSchema = StructType([StructField("FileName", StringType())])
+        wav_DF = spark.createDataFrame(wav_files, StructType([StructField("FileName", StringType())]))
+        wav_DF.show()
+        
         #data_schema = [StructField('Patient_Number',IntegerType(),True)]
         #    column = wav_files[0].split('_')
       
