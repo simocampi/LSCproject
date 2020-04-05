e18ed2e3dfe6d510e4d034082ceeec0dbd46268f
simocampi <37673011+simocampi@users.noreply.github.com>
2020-04-05 13:54:43 +0200

    waveDataframe

--- wav_manipulation/wav.py
+++ wav_manipulation/wav.py
@@ -5,22 +5,18 @@ from pyspark.sql.types import (StructField,StringType,IntegerType,StructType)
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit
 
-spark = SparkSession \
-    .builder \
-    .appName("Python Spark Exercise 5") \
-    .getOrCreate()
-
-class WAV:
+class WAV(object):
     
     PATH_FILES_WAV = Path.get_wav_file_path()
     
+    def __init__(self,spark_session):
+        self.spark_session = spark_session
 
-    @staticmethod
-    def wav_files():
+    def wav_files(self):
         wav_files = [[f] for f in listdir(WAV.PATH_FILES_WAV) if (isfile(join(WAV.PATH_FILES_WAV, f)) and f.endswith('.wav'))] 
 
-        cSchema = StructType([StructField("FileName", StringType())])
-        wav_DF = spark.createDataFrame(wav_files, StructType([StructField("FileName", StringType())]))
+        #cSchema = StructType([StructField("FileName", StringType())])
+        wav_DF = self.spark_session.createDataFrame(wav_files, StructType([StructField("FileName", StringType())]))
 
         wav_DF = wav_DF.withColumn('Patient_ID', lit(None).cast(StringType()))
         wav_DF = wav_DF.withColumn('Recording_idx', lit(None).cast(StringType()))
@@ -30,9 +26,19 @@ class WAV:
 
         wav_DF.printSchema()
         wav_DF.show()
-        
-        #data_schema = [StructField('Patient_Number',IntegerType(),True)]
-        #    column = wav_files[0].split('_')
+    
+    def wav_dataframe(self):  
+        wav_files = [[f] for f in listdir(WAV.PATH_FILES_WAV) if (isfile(join(WAV.PATH_FILES_WAV, f)) and f.endswith('.wav'))]   
+        data_schema = [ StructField('Patient_ID',IntegerType(),True),
+                        StructField('Recording_idx',StringType(),True),
+                        StructField('Chest_Location',StringType(),True),
+                        StructField('Acquisition_Mode',StringType(),True),
+                        StructField('Recording_Equipement',StringType(),True)
+                        ]
+        struct_dataschema = StructType(fields=data_schema)
+        df = self.spark_session.createDataFrame([],struct_dataschema)
+        df.printSchema()
+        df.show()
       
     
 
