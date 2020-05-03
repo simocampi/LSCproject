d6b9f00d18785ae92f5fb37c0a5f779e5f759254
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-05-03 11:44:49 +0200

    non va ancora

--- wav_manipulation/wav.py
+++ wav_manipulation/wav.py
@@ -7,6 +7,7 @@ from pyspark.sql.types import (StructField,StringType,IntegerType,StructType,Flo
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import split, substring, col, regexp_replace, reverse
 from pyspark.sql.functions import lit
+import wave
 from pyspark.sql.functions import *
 from time import time
 import pandas as pd
@@ -18,11 +19,17 @@ class WAV(object):
     
     PATH_FILES_WAV = Path.get_wav_file_path()
     
-    def __init__(self,spark_session):
+    def __init__(self,spark_session,spark_context):
         self.spark_session = spark_session
+        self.spark_context = spark_context
+
+    def read_was_as_binary(self,spark_context):
+        binary_wave = spark_context.binaryFiles(Path.get_wav_file_path()+'222_1b1_Pr_sc_Meditron.wav')
+        return binary_wave
 
     def recording_info(self):
         wav_files = self.get_fileNames_test()
+        print(type(wav_files), len(wav_files), type(wav_files[0]), len(wav_files[0]), wav_files[0])
 
         wav_DF = self.spark_session.createDataFrame(wav_files, StructType([StructField("FileName", StringType(), False)]))
 
@@ -34,7 +41,7 @@ class WAV(object):
         wav_DF = wav_DF.withColumn("Recording_Equipement", split_col.getItem(4))
 
         wav_DF.printSchema()
-        wav_DF.show(5)
+        #wav_DF.show(2, False)
 
     def recording_annotation(self):
         idx_fileName = len(WAV.PATH_FILES_WAV.split(Path.path_separator))
@@ -46,16 +53,14 @@ class WAV(object):
                             StructField("Wheezes", IntegerType(), True)]
 
         data_structure = StructType(original_schema)
-
+        print(WAV.PATH_FILES_WAV)
         df = self.spark_session.read.\
             csv(path=WAV.PATH_FILES_WAV+'*.txt', header=False, schema= data_structure, sep='\t').\
             withColumn("Filename", split(input_file_name(), "/").getItem(idx_fileName) ).\
             withColumn("duration", col("End") - col("Start"))
 
-        
-
-        df.show(5, False)
         df.printSchema()
+        df.show(2, False)
 
 
     
@@ -66,26 +71,24 @@ class WAV(object):
         try:
             #IF THE FILE ALREDY EXIST
             indexingFiles = self.openIndexingFiles(folder_path=path)
-            print(indexingFiles)
             for line in indexingFiles:
-                list_of_fileName.append(line)
+                list_of_fileName.append([line])
         except IOError:
             print("\nIndexing file for path \'{}\' not present, creating it...".format(path))
             list_of_fileName = self.createIndexingFile_andGetContent(folder_path=path)
-        finally:
-            print("ciao")
-            #indexingFiles.close()
-        return [list_of_fileName]
+
+        print(type(list_of_fileName), len(list_of_fileName), type(list_of_fileName[0]), len(list_of_fileName[0]), list_of_fileName[0])
+        return list_of_fileName
     
 
     def openIndexingFiles(self, folder_path):
         if Path.RunningOnLocal:
             #WINDOWS LOCAL MACHINE
-            f = open(folder_path+'index_fileName.txt', 'r')
+            f = open(folder_path+'index_fileName', 'r')
             return f
         else:
             #UNIGE CLUSTER SERVER
-            args = "hdfs dfs -cat "+folder_path+"index_fileName.txt"
+            args = "hdfs dfs -cat "+folder_path+"index_fileName"
             proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             stdout, stderr = proc.communicate()
 
@@ -98,12 +101,14 @@ class WAV(object):
         list_of_fileName = []
         if Path.RunningOnLocal:
             #WINDOWS LOCAL MACHINE
-            list_of_fileName = [f[:-4] for f in listdir(folder_path) if (isfile(join(folder_path, f)) and f.endswith('.txt'))]
+            list_of_fileName = [[f[:-4]] for f in listdir(folder_path) if (isfile(join(folder_path, f)) and f.endswith('.txt'))]
 
-            indexingFiles = open(folder_path+'index_fileName.txt','w')
+            indexingFiles = open(folder_path+'index_fileName','w')
             for fileName in list_of_fileName:
-                indexingFiles.write(fileName+"\n")
+                indexingFiles.write(fileName[0])
+                indexingFiles.write("\n")
             indexingFiles.close()
+            
         else:
             #UNIGE CLUSTER SERVER
             args = "hdfs dfs -ls "+folder_path+"*.txt | awk '{print $8}'"
@@ -111,22 +116,20 @@ class WAV(object):
 
             s_output, s_err = proc.communicate()
             tmp_list = s_output.split()
-            print(Path.path_separator)
-            print(Path.RunningOnLocal)
             for line in tmp_list:
                 fileName = line.split("/")[-1]
-                list_of_fileName.append(fileName[:-4])
+                list_of_fileName.append([fileName[:-4]])
 
             #save file in hadoop file system
             tmpFile = open('tmp','w')
             for fileName in list_of_fileName:
-                tmpFile.write(fileName+"\n")
+                tmpFile.write(fileName[0]+"\n")
             tmpFile.close()
 
-            args = "hdfs dfs -put tmp "+folder_path+"index_fileName.txt"
+            args = "hdfs dfs -put tmp "+folder_path+"index_fileName"
             proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             #os.remove('tmp')
-
+        print(type(list_of_fileName), len(list_of_fileName), type(list_of_fileName[0]), len(list_of_fileName[0]), list_of_fileName[0])
         return list_of_fileName
 
         
