2e8e18002688ac4cf82297f25babd4a0100dbef3
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-05-03 16:37:45 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+
+#remove compiled files
 find . -name "*.pyc" -type f -delete
-ssh user24@192.168.20.157 "cd LSCproject/; rm -rf DataManipulation/ *.py* Utils/ wav_manipulation/"
+
+#spawn ssh user24@192.168.20.157 "cd LSCproject/; rm -rf DataManipulation/ *.py* Utils/ wav_manipulation/"
+expect -c 'spawn ssh user24@192.168.20.157 "cd LSCproject/; rm -rf DataManipulation/ *.py* Utils/ wav_manipulation/";
+expect "assword:";
+send "user24\r";
+interact'
+
 echo "Removed old files."
 echo
 
-scp -r DataManipulation/ Main.py Utils/ wav_manipulation/ user24@192.168.20.157:LSCproject && echo "Uploaded all new files"
+#scp -r DataManipulation/ Main.py Utils/ wav_manipulation/ user24@192.168.20.157:LSCproject
+
+expect -c 'spawn scp -r DataManipulation/ Main.py Utils/ wav_manipulation/ user24@192.168.20.157:LSCproject;
+expect "assword:";
+send "user24\r";
+interact'
+#expect -c 'scp -r DataManipulation/ Main.py Utils/ wav_manipulation/ user24@192.168.20.157:LSCproject;
+#expect "assword:";
+#send "user24\r";
+#interact'
