d5b349190df9199abf8289909fe152a0a925c80b
Alessandro <alessandro.caroti.1996@gmail.com>
2020-08-20 12:00:46 +0200

    a

---
+++ Utils/miscellaneous.py
@@ -0,0 +1,11 @@
+def split_train_test(all_data):
+    train_data, test_data = scaled_df.randomSplit([.8,.2],seed=1234)
+
+    return train_data, test_data
+
+
+def divide_in_label_and_feature(rdd):
+
+    input_data = rdd.map(lambda x: (x[0], DenseVector(x[1:])))
+
+    df = spark.createDataFrame(input_data, ["label", "features"])
